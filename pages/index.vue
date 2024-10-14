f7b700245767bd796d66640b43d0f4576a381a12
martoast <alexmartos96@gmail.com>
2024-10-14 12:43:00 -0700

    seo meta

--- pages/index.vue
+++ pages/index.vue
@@ -15,4 +15,24 @@
       <Contact/>
     </div>
   </template>
+
+<script setup>
+
+useSeoMeta({
+        robots: 'index, follow',
+        title: 'Tranquility Haven',
+        ogTitle: 'Tranquility Haven',
+        author: 'Alex Martos',
+        charset: 'utf-8',
+        description: "A nurturing community where seniors and veterans can thrive, connect, and find the care they deserve.",
+        ogDescription: "A nurturing community where seniors and veterans can thrive, connect, and find the care they deserve.",
+        appleMobileWebAppCapable: 'yes',
+        mobileWebAppCapable: 'yes',
+        ogImage: '/diversity.jpg',
+        ogLocaleAlternate: ['es_LA'],
+        ogType: 'website',
+        viewport: 'width=device-width, initial-scale=1',
+    })
+
+</script>
   
